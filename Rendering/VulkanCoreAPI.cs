0d627536a4ccf16ab046a7639aae3ef492a15f59
Alexander Meissner <alexander.meissner@rwth-aachen.de>
2023-03-12 15:36:18 +0100

    interop api

---
+++ Rendering/VulkanCoreAPI.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Rendering
+{
+    public static partial class VulkanCoreAPI
+    {
+        internal static partial class NativeVulkanCore
+        {
+            [LibraryImport("VulkanCore.dll")]
+            [UnmanagedCallConv(CallConvs = new System.Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static partial bool Initialize();
+        }
+
+        public static bool Initialize()
+        {
+            return NativeVulkanCore.Initialize();
+        }
+    }
+}
